4c2ec92c71ba6f4dc3d37cf12948b02966433d42
varshi-code <varshithgoud2786@gmail.com>
2026-09-11 21:53:11 +0530

    Complete sales prediction task

---
+++ sales_prediction.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+df = pd.read_csv("Advertising.csv")
+
+print(df.head())
+print("\nDataset Shape:")
+print(df.shape)
+
+print("\nDataset Information:")
+print(df.info())
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+print("\nStatistical Summary:")
+print(df.describe())
+df = df.drop_duplicates()
+
+print("\nMissing Values After Cleaning:")
+print(df.isnull().sum())
+
+print("\nShape After Cleaning:")
+print(df.shape)
+
+X = df[["TV", "Radio", "Newspaper"]]
+y = df["Sales"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+print("Training data:", X_train.shape)
+print("Testing data:", X_test.shape)
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print("Model training completed!")
+y_pred = model.predict(X_test)
+y_pred = model.predict(X_test)
+
+print("\nPredicted Sales:")
+print(y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+print("\nModel Evaluation:")
+print("MAE:", mae)
+print("MSE:", mse)
+print("RMSE:", rmse)
+print("R2 Score:", r2)
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Sales")
+plt.ylabel("Predicted Sales")
+plt.title("Actual vs Predicted Sales")
+plt.show()
